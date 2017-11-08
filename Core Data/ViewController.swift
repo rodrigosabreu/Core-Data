27ea562dcfe941750d31a5229bab39419cb0fc63
Rodrigo Abreu <rodrigoabreu@rodrigos-macbook-pro.local>
2017-11-08 15:52:37 -0200

    salvando dados

--- Core Data/ViewController.swift
+++ Core Data/ViewController.swift
@@ -7,12 +7,34 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        
+        /*Criar entidade*/
+        let usuario = NSEntityDescription.insertNewObject(forEntityName: "Usuario", into: context)
+        
+        /*Configurar Objeto*/
+        usuario.setValue("Tatiana Abreu", forKey: "nome")
+        usuario.setValue(34, forKey: "idade")
+        usuario.setValue("tatiana.abreu", forKey: "login")
+        usuario.setValue("1234", forKey: "senha")
+        
+        //Salvar (persistir) os dados
+        do {
+            try context.save()
+            print("Dados salvos corretamente!")
+        } catch  {
+            print("Erro ao salvar os dados")
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
